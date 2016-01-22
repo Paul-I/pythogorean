9f8864f6be330bb8154656afa77e6e4e2aee81bf
Paul Ijagbemi <paulijagbemi@seoulchristian.org>
2016-01-22 10:09:34 +0900

    pythogorean.py

--- pythogorean.py
+++ pythogorean.py
@@ -1,5 +1,8 @@
+#Float raw input is what allows the user to enter the number
 a = float(raw_input("What is the length of the first side? : "))
 b = float(raw_input("What is the length of the second side? : "))
+#This is the main equation, this is how the program solve the problem
 C_squared = a**2 + b**2
 c = C_squared**0.5
+#This is what tells the user the answer.
 print "The length of the hypotenuse is %r" % c
