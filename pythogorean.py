232c5b0200bbea4fded69e668284e152317b35f7
Paul Ijagbemi <paulijagbemi@seoulchristian.org>
2016-01-22 10:04:27 +0900

    pythogorean.py

---
+++ pythogorean.py
@@ -0,0 +1,5 @@
+a = float(raw_input("What is the length of the first side? : "))
+b = float(raw_input("What is the length of the second side? : "))
+C_squared = a**2 + b**2
+c = C_squared**0.5
+print "The length of the hypotenuse is %r" % c
